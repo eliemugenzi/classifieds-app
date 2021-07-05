fe13607652b00f5f6532642307c5fd5b58756e9b
Elie Mugenzi <eliemugenzi@gmail.com>
2021-07-05 19:59:18 +0200

    Change base api url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export default {
-  API_URL: 'https://classifieds-backend.herokuapp.com//api/v1',
+  API_URL: 'https://classifieds-backend.herokuapp.com/api/v1',
   AWS_API_KEY: 'AKIAJOZVWGCDBNVWBJRA',
   AWS_SECRET_KEY: '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',
   AWS_STORAGE_BUCKET: 'mola',
